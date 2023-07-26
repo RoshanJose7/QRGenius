e2a42c0c6c967e0fd1843827388580a03dbc05ea
RoshanJose <roshanstark007@gmail.com>
2023-07-26 14:50:02 +0530

    Add: Additional modification options

--- src/App.tsx
+++ src/App.tsx
@@ -8,114 +8,153 @@ import { ChangeEvent, useEffect, useRef, useState } from "react";
 
 import "./styles.css";
 
-const qrCode = new QRCodeStyling({
-  width: 500,
-  height: 500,
-  data: "https://ostello.co.in",
-  imageOptions: {
-    crossOrigin: "anonymous",
-    margin: 20,
-    hideBackgroundDots: true,
-    imageSize: 0.4,
-  },
-  dotsOptions: { type: "classy", color: "#7D23E0" },
-  backgroundOptions: { color: "#ffffff" },
-  cornersSquareOptions: { type: "dot", color: "#000000" },
-  cornersDotOptions: { type: "square", color: "#000000" },
-});
-
 export default function App() {
   const ref = useRef<HTMLDivElement | null>(null);
+  const [qrCode, setQrCode] = useState(
+    new QRCodeStyling({
+      width: 300,
+      height: 300,
+      data: "https://google.co.in",
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20,
+        hideBackgroundDots: true,
+        imageSize: 0.4,
+      },
+      dotsOptions: { type: "classy", color: "#7D23E0" },
+      backgroundOptions: { color: "#ffffff" },
+      cornersSquareOptions: { type: "extra-rounded", color: "#000000" },
+      cornersDotOptions: { type: "square", color: "#000000" },
+    })
+  );
 
   useEffect(() => {
     qrCode.append(ref.current!);
-  }, []);
-
-  function fileToDataURL(file: File): Promise<string> {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = () => resolve(reader.result as string);
-      reader.onerror = (error) => reject(error);
-      reader.readAsDataURL(file);
-    });
-  }
+  }, [qrCode]);
 
   // URL Options
-  const [url, setUrl] = useState("https://ostello.co.in");
+  const [url, setUrl] = useState("https://google.co.in");
 
   const onUrlChange = async (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
     setUrl(event.target.value);
+
+    setQrCode((prev) => {
+      prev.update({
+        data: event.target.value,
+      });
+
+      return prev;
+    });
   };
 
-  // Image Options
-  const [imageFile, setImageFile] = useState<string>("");
-  const [imageSize, setImageSize] = useState<number>(0.4);
-  const [imageMargin, setImageMargin] = useState<number>(0);
-
-  const onImageFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setImageFile(await fileToDataURL(event.target.files![0]));
-  };
-
-  const onImageSizeChange = (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setImageSize(Number.parseInt(event.target.value));
-  };
-
-  const onImageMarginChange = (event: ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setImageMargin(Number.parseInt(event.target.value));
-  };
-
-  // Dot Options
-  const [dotType, setDotType] = useState<string>("classy");
+  // QR Code Options
+  const [dotType, setDotType] = useState<DotType>("classy");
   const [dotColor, setDotColor] = useState<string>("#7D23E0");
+  const [backgroundColor, setBackgroundColor] = useState<string>("#ffffff");
+  const [cornerSquareType, setCornerSquareType] =
+    useState<CornerSquareType>("square");
+  const [cornerSquareColor, setCornerSquareColor] = useState<string>("#000000");
+  const [cornerDotType, setCornerDotType] = useState<CornerDotType>("square");
+  const [cornerDotColor, setCornerDotColor] = useState<string>("#000000");
 
   const onDotTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
-    setDotType(event.target.value);
+    setDotType(event.target.value as DotType);
+
+    setQrCode((prev) => {
+      prev.update({
+        dotsOptions: {
+          type: event.target.value as DotType,
+        },
+      });
+
+      return prev;
+    });
   };
 
   const onDotColorChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setDotColor(event.target.value);
-  };
+    setDotColor(event.target.value as string);
 
-  // Background Options
-  const [backgroundColor, setBackgroundColor] = useState<string>("#ffffff");
+    setQrCode((prev) => {
+      prev.update({
+        dotsOptions: {
+          color: event.target.value as string,
+        },
+      });
+
+      return prev;
+    });
+  };
 
   const onBackgroundColorChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setBackgroundColor(event.target.value);
-  };
+    setBackgroundColor(event.target.value as string);
 
-  // Corner Square Options
-  const [cornerSquareType, setCornerSquareType] = useState<string>("dot");
-  const [cornerSquareColor, setCornerSquareColor] = useState<string>("#000000");
+    setQrCode((prev) => {
+      prev.update({
+        backgroundOptions: { color: event.target.value as string },
+      });
+
+      return prev;
+    });
+  };
 
   const onCornerSquareTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
-    setCornerSquareType(event.target.value);
+    setCornerSquareType(event.target.value as CornerSquareType);
+
+    setQrCode((prev) => {
+      prev.update({
+        cornersSquareOptions: { type: event.target.value as CornerSquareType },
+      });
+
+      return prev;
+    });
   };
 
   const onCornerSquareColorChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setCornerSquareColor(event.target.value);
-  };
+    setCornerSquareColor(event.target.value as string);
 
-  // Corner Dot Options
-  const [cornerDotType, setCornerDotType] = useState<string>("square");
-  const [cornerDotColor, setCornerDotColor] = useState<string>("#000000");
+    setQrCode((prev) => {
+      prev.update({
+        cornersSquareOptions: {
+          color: event.target.value as string,
+        },
+      });
+
+      return prev;
+    });
+  };
 
   const onCornerDotTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
     event.preventDefault();
-    setCornerDotType(event.target.value);
+    setCornerDotType(event.target.value as CornerDotType);
+
+    setQrCode((prev) => {
+      prev.update({
+        cornersDotOptions: { type: event.target.value as CornerDotType },
+      });
+
+      return prev;
+    });
   };
 
   const onCornerDotColorChange = (event: ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setCornerDotColor(event.target.value);
+    setCornerDotColor(event.target.value as string);
+
+    setQrCode((prev) => {
+      prev.update({
+        cornersDotOptions: {
+          color: event.target.value as string,
+        },
+      });
+
+      return prev;
+    });
   };
 
   // Download Options
@@ -131,43 +170,10 @@ export default function App() {
     });
   };
 
-  useEffect(() => {
-    qrCode.update({
-      data: url,
-      image: imageFile,
-      imageOptions: {
-        margin: imageMargin,
-        imageSize: imageSize,
-      },
-      dotsOptions: { type: dotType as DotType, color: dotColor },
-      backgroundOptions: { color: backgroundColor },
-      cornersSquareOptions: {
-        type: cornerSquareType as CornerSquareType,
-        color: cornerSquareColor,
-      },
-      cornersDotOptions: {
-        type: cornerDotType as CornerDotType,
-        color: cornerDotColor,
-      },
-    });
-  }, [
-    url,
-    imageFile,
-    imageMargin,
-    imageSize,
-    dotType,
-    dotColor,
-    backgroundColor,
-    cornerSquareType,
-    cornerSquareColor,
-    cornerDotType,
-    cornerDotColor,
-  ]);
-
   return (
     <div className="main">
       <div style={styles.inputWrapper}>
-        <div className="url-options">
+        <div className="options">
           <h4>URL Options</h4>
 
           <label htmlFor="url-input-value">Link to Redirect</label>
@@ -179,129 +185,106 @@ export default function App() {
           />
         </div>
 
-        <div className="image-options">
-          <h4>Image Options</h4>
+        <div className="options">
+          <h4>QR Code Options</h4>
 
-          <label htmlFor="image-file">Image File</label>
-          <input name="image-file" type="file" onChange={onImageFileChange} />
+          <div>
+            <label htmlFor="dot-type">Dot Options </label>
+            <select name="dot-type" onChange={onDotTypeChange} value={dotType}>
+              <option value="dots">Dots</option>
+              <option value="rounded">Rounded</option>
+              <option value="classy">Classy</option>
+              <option value="classy-rounded">Classy Rounded</option>
+              <option value="square">Square</option>
+              <option value="extra-rounded">Extra Rounded</option>
+            </select>
+          </div>
 
-          <label htmlFor="image-size">Image Size</label>
-          <input
-            name="image-size"
-            value={imageSize}
-            type="range"
-            min="0.1"
-            max="1"
-            onChange={onImageSizeChange}
-          />
+          <div>
+            <label htmlFor="dot-color">Dot Color </label>
+            <input
+              type="color"
+              name="dot-color"
+              onChange={onDotColorChange}
+              value={dotColor}
+            />
+          </div>
 
-          <label htmlFor="image-margin">Image Margin</label>
-          <input
-            name="image-margin"
-            value={imageMargin}
-            type="range"
-            min="0"
-            max="20"
-            onChange={onImageMarginChange}
-          />
+          <div>
+            <label htmlFor="background-color">Background Color </label>
+            <input
+              type="color"
+              name="background-color"
+              onChange={onBackgroundColorChange}
+              value={backgroundColor}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="corner-square-type">Corner Square Type </label>
+            <select
+              name="corner-square-type"
+              onChange={onCornerSquareTypeChange}
+              value={cornerSquareType}
+            >
+              <option value="dot">Dot</option>
+              <option value="square">Square</option>
+              <option value="extra-rounded">Extra Rounded</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="corner-square-color">Corner Square Color </label>
+            <input
+              type="color"
+              name="corner-square-color"
+              onChange={onCornerSquareColorChange}
+              value={cornerSquareColor}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="corner-dot-type">Corner Dot Type </label>
+            <select
+              name="corner-dot-type"
+              onChange={onCornerDotTypeChange}
+              value={cornerDotType}
+            >
+              <option value="dot">Dot</option>
+              <option value="square">Square</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="corner-dot-color">Corner Dot Color </label>
+            <input
+              type="color"
+              name="corner-square-color"
+              onChange={onCornerDotColorChange}
+              value={cornerDotColor}
+            />
+          </div>
         </div>
 
-        <div className="dot-options">
-          <h4>Dot Options</h4>
-
-          <label htmlFor="dot-type">Dot Type</label>
-          <select name="dot-type" onChange={onDotTypeChange} value={dotType}>
-            <option value="classy">Classy</option>
-            <option value="dots">Dots</option>
-            <option value="rounded">Rounded</option>
-            <option value="classy-rounded">Classy Rounder</option>
-            <option value="square">Square</option>
-            <option value="extra-rounded">Extra Rounded</option>
-          </select>
-
-          <label htmlFor="dot-color">Dot Color</label>
-          <input
-            value={dotColor}
-            name="dot-color"
-            type="color"
-            onChange={onDotColorChange}
-          />
-        </div>
-
-        <div className="background-options">
-          <h4>Background Options</h4>
-
-          <label htmlFor="background-color">Background Color</label>
-          <input
-            name="background-color"
-            type="color"
-            value={backgroundColor}
-            onChange={onBackgroundColorChange}
-          />
-        </div>
-
-        <div className="corner-square-options">
-          <h4>Corner Square Options</h4>
-
-          <label htmlFor="corner-square-type">Corner Square Type</label>
-          <select
-            name="corner-square-type"
-            onChange={onCornerSquareTypeChange}
-            value={cornerSquareType}
-          >
-            <option value="dot">Classy</option>
-            <option value="square">Square</option>
-            <option value="extra-rounded">Extra Rounder</option>
-          </select>
-
-          <label htmlFor="corner-square-color">Corner Square Color</label>
-          <input
-            name="corner-square-color"
-            type="color"
-            value={cornerSquareColor}
-            onChange={onCornerSquareColorChange}
-          />
-        </div>
-
-        <div className="corner-dot-options">
-          <h4>Corner Dot Options</h4>
-
-          <label htmlFor="corner-dot-type">Corner Dot Type</label>
-          <select
-            name="corner-dot-type"
-            onChange={onCornerDotTypeChange}
-            value={cornerDotType}
-          >
-            <option value="dot">Classy</option>
-            <option value="square">Square</option>
-          </select>
-
-          <label htmlFor="corner-dot-color">Corner Dot Color</label>
-          <input
-            name="corner-dot-color"
-            type="color"
-            value={cornerDotColor}
-            onChange={onCornerDotColorChange}
-          />
-        </div>
-
-        <div className="download-options">
+        <div className="options">
           <h4>Download Options</h4>
-
           <label htmlFor="download-options-file-extension">
             File Download Options
           </label>
-          <select
-            name="download-options-file-extension"
-            onChange={onExtensionChange}
-            value={fileExt}
-          >
-            <option value="png">PNG</option>
-            <option value="jpeg">JPEG</option>
-            <option value="webp">WEBP</option>
-          </select>
 
-          <button onClick={onDownloadClick}>Download</button>
+          <div style={styles.inputBox}>
+            <select
+              name="download-options-file-extension"
+              onChange={onExtensionChange}
+              value={fileExt}
+            >
+              <option value="png">PNG</option>
+              <option value="jpeg">JPEG</option>
+              <option value="webp">WEBP</option>
+            </select>
+
+            <button onClick={onDownloadClick}>Download</button>
+          </div>
         </div>
       </div>
 
@@ -318,7 +301,6 @@ const styles = {
     width: "95%",
   },
   inputBox: {
-    flexGrow: 1,
-    marginRight: 20,
+    marginTop: "10px",
   },
 };
